9df2c19f86b6327b9be3d23f9d8a549d5ef1a1cc
salih <salihedneer.mec@gmail.com>
2025-11-02 16:11:55 +0530

    clean

--- backend/src/utils/url.js
+++
@@ -1,3 +0,0 @@
-https://book-story-ai-generate.s3.amazonaws.com/books/chaim-celebrates-the-chagim-1761756792298/dedication-page/background-1761766367296.png?cb=2025-11-02T06%3A02%3A29.701Z-background-2
-
-https://book-story-ai-generate.s3.amazonaws.com/books/chaim-celebrates-the-chagim-1761756792298/story/characters/1-1762063232389-6906f3464de5a5f00e4bea25-1.png?cb=2025-11-02T06%3A02%3A29.702Z-preload-char
